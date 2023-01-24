912b0cc7c96483d3319b3553de1cebd1d80bb922
Anshuman202 <119556991+Anshuman202@users.noreply.github.com>
2023-01-24 18:35:55 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿namespace AddressBook
+namespace AddressBook
 {
     class Program
     {
         public static void Main(string[] args)
         {
-
-
+            
+            
             Operations operations = new Operations();
             operations.addPerson();
-
+            operations.display();
+            operations.edit();
+            operations.delete();
         }
     }
 }
