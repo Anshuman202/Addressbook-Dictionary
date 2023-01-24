7ad17b47f73cb27ad9917266d95aff1dcb05e412
Anshuman202 <119556991+Anshuman202@users.noreply.github.com>
2023-01-24 18:48:18 +0530

    to take the inputs model class

---
+++ Person.cs
@@ -0,0 +1,17 @@
+namespace AddressBook
+{
+    public class Person
+    {
+        public string fname { get; set; }
+        public string lname { get; set; }
+       
+        public string addr { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+        public string zip { get; set; }
+
+        public string phnumber { get; set; }
+        public string mail { get; set; }
+
+    }
